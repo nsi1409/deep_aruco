aad8bdf2c5f3987d8c867b425d2e3a388f420371
Evan <obrienem@rose-hulman.edu>
2022-06-27 22:00:04 -0400

    loss function needs to be changed for new format

--- ldr.py
+++ ldr.py
@@ -5,6 +5,8 @@ from torchvision import transforms
 from PIL import Image
 import torchvision.models as md
 import torch.nn as nn
+import torch.optim as optim
+
 
 convert_tensor = transforms.ToTensor()
 
@@ -35,12 +37,6 @@ def index_load(i):
 	img_ten = convert_tensor(img)
 	return img_ten, label_ten
 
-vec_i = index_load(6)
-print(f'test vec: {vec_i}')
-
-print(f'test indices: {data_indexed[0]["regions"]}')
-
-print(f'dataset length {len(data_indexed)}')
 
 class CustomDataset(torch.utils.data.Dataset):
 	def __init__(self):
@@ -54,25 +50,64 @@ class CustomDataset(torch.utils.data.Dataset):
 
 
 dataset = CustomDataset()
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=4,
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=2,
                         shuffle=True, num_workers=0)
-
-img, label = dataloader
-
-print(f'dataset test indx 0: {dataset[0]}')
-print(f'dataloader test indx 0: {img}')
 
 
 res = md.resnet50(pretrained=True)
 model = nn.Sequential(res, nn.Linear(1000, number_tags*9))
-print(res)
+#print(res)
 
 
 inpt, _ = dataset[0]
 resize = transforms.Resize(224)
 inpt = resize(inpt).unsqueeze(0)
-print(f'inpt: {inpt}')
+#print(f'inpt: {inpt}')
 
-print(f'resnet50 test: {model(inpt)}')
+#print(f'resnet50 test: {model(inpt)}')
+
+
+def aruco_loss(test, base):
+	loss = 0
+	loss_ten = torch.tensor([0.0])
+	for i in range(base.size(dim=0)):
+		base_class = base[i][0].item()
+		test_class = test[i][0].item()
+
+		base_class_ten = base[i][0]
+		test_class_ten = test[i][0]
+
+		loss += (base_class - test_class) ** 2
+		loss_ten += (base_class_ten - test_class_ten) ** 2
+
+		run = 0
+		run_ten = torch.tensor([0.0])
+		for j in range(base.size(dim=1) - 1):
+			#print(f'base class {base_class} , test class {test_class}')
+			run += (base[i][j+1].item() - test[i][j+1].item()) ** 2
+			run_ten += (base[i][j+1] - test[i][j+1]) ** 2
+			#print(f'run loss: {run}, test scalar: {test[i][j]}, base scalar: {base[i][j]}')
+		loss += ((base_class * test_class) ** 2) * (run / (base.size(dim=1) - 1))
+		loss_ten += ((base_class_ten * test_class_ten) ** 2) * (run_ten / torch.tensor([(base.size(dim=1) - 1)]).float())
+
+	return loss_ten
+
+
+optimizer = optim.Adam(model.parameters(), lr=0.05)
+
+print("enumerate start")
+for epoch in range(1):
+	for indx, samples in enumerate(dataloader):
+		#print(indx, samples)
+		imgs, labels = samples
+		imgs = resize(imgs)
+		optimizer.zero_grad()
+		output = model(imgs)
+		print(output.size())
+		loss = aruco_loss(output, labels)
+		loss.backward()
+		optimizer.step()
+		#print(f'loss: {loss}')
+		#print(model.weight.grad)
 
 
